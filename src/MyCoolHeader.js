4345336adce43f2952705bfbf0b91da265d2b115
sformichella <sformichella@gmail.com>
2020-10-26 16:14:49 -0700

    separate stuff in separate files

---
+++ src/MyCoolHeader.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class MyCoolHeader extends React.Component {
+    render() {
+        return (
+          <header>
+            <div className="left-side-header">
+              <img src="logo.png" alt="fork and knife"/>
+              <div>
+                <h1>Delicious</h1>
+                <p>The best food blog on the web.</p>
+                </div>
+            </div>
+            <div className="right-side-header">
+              <img src="fb-icon.png"alt="facebook" />
+              <img src="twit-icon.png"alt="twitter" />
+              <img src="gp-icon.png"alt="google plus" />
+              <img src="insta-icon.png"alt="instagram" />
+              <img src="flic-icon.png"alt="flic icon" />
+              <img src="pint-icon.png"alt="pintrest icon" />
+              <img src="rss-icon.png"alt="rss icon" />
+              <img src="mail-icon.png"alt="mail icon" />
+            </div>
+          </header>
+        );
+    }
+  }
