c89e00c5a35b6e581437754ab3a094068d486034
sarah-svg <kjskillen@gmail.com>
2020-10-26 14:32:07 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyCoolHeader extends React.Component {
+  render() {
+      return (
+          <div> Hi, {this.props.name}, I am the header!</div>
+      );
+  }
 }
 
-export default App;
+class MyAmazingArticle extends React.Component {
+  render() {
+      return (
+          <div>I am the article!</div>
+      );
+  }
+}
+
+class MySpecialFooter extends React.Component {
+  render() {
+      return (
+          <div> I am the footer! Call us at {this.props.phone}</div>
+      );
+  }
+}
+
+export default class PutEmAllTogether extends React.Component {
+  render() {
+      return (
+        <div>
+        <h1>Time to click the legos together!</h1>
+        
+          <MyCoolHeader name="dani" />
+          <MyAmazingArticle />
+          <MySpecialFooter phone="345-456-5431" />
+        </div>
+      );
+  }
+}
