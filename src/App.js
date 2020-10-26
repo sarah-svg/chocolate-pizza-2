f4b3aeab8b02a45f1431b04fc712369d8ffb2d17
sarah-svg <kjskillen@gmail.com>
2020-10-26 16:45:52 -0700

    captions

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import MyCoolHeader from './MyCoolHeader.js';
 import MyAmazingArticle from './MyAmazingArticle.js';
 import RecipeList from './RecipeList.js';
 import MySpecialFooter from './MySpecialFooter.js';
+import ParagraphDiv from './ParagraphDiv.js'
 
 export default class PutEmAllTogether extends React.Component {
   render() {
       return (
         <main>  
-          <MyCoolHeader name="dani" />
-          <MyAmazingArticle />
+          <MyCoolHeader title="Delicious"/>
+          <MyAmazingArticle name="Chocolate Pizza"/>
+          <ParagraphDiv />
           <RecipeList />
-          <MySpecialFooter phone="345-456-5431" />
+          <MySpecialFooter author="Vanessa Stevenson" />
         </main>
       );
   }
