93e03972d3d7576d1a0b5d4325ae9989fefdef21
sformichella <sformichella@gmail.com>
2020-10-26 15:32:09 -0700

    add recipe list

--- src/App.js
+++ src/App.js
@@ -20,30 +20,27 @@ class MyCoolHeader extends React.Component {
             <img src="rss-icon.png"alt="rss icon" />
             <img src="mail-icon.png"alt="mail icon" />
           </div>
-          
         </div>
       );
   }
 }
+
 class MyAmazingArticle extends React.Component {
   render() {
       return (
         <div className="section-header">
         <h2>Chocolate Pizza</h2>
         <p>Posted on 15 Dec 2013 / Desserts</p>
-        <img src="choco-pizza.png" alt="chocolate pizza" />
-            
+        <img src="choco-pizza.png" alt="chocolate pizza" />   
         <p>
             For the fig-swirl: Melt butter over medium heat in a saucepan. Add brown sugar and stir to dissolve. Have All
             of the figs and toss in the saucepan with water and lemon juice. Cook over medium heat, stirring frequently, until
             you have a chunky-jammy mixture. Add salt with one or two stirs, set aside and let cool completely.
         </p>
-
         <p>
             Ice cream: In a small pot over medium heat, combine milk and granulated sugar until sugar is completely dissolved and the milk is just barely lukewarm.
             Whisk in the egg yolks. Set mixture in the fridge and wait until the fig mixture in cooled.
         </p>
-
         <p>
             Using an ice cream machine, pour liquids into the frozen basin and process according to the manufacturer instructions, i.e. let spin and thicken for 20 minutes before adding mascrarpone, fig jam mixture, and the nuts.
             Continue to process fpr +/- 10 minutes. Pour semi-frozen mixture into a pyrex dish or glass tupperware. Freeze for at least two hours before serving.
@@ -52,6 +49,32 @@ class MyAmazingArticle extends React.Component {
       );
   }
 }
+
+class RecipeList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>1 1/2 cups milk</li>
+          <li> 1/2 cups mascrarpone</li>
+          <li> 1/2 tsp pink salt</li>
+          <li>1lb Black Mission Figs</li>
+          <li> 1/2 cup brown sugar</li>
+          <li>2-4 tbsp water</li>
+        </ul>
+        <ul>
+          <li>1 1/2 cups heavy cream</li>
+          <li>1/3 granulated sugar</li>
+          <li>2 egg yolks</li>
+          <li>lemon, juice</li>
+          <li>2 tbsp butter</li>
+          <li>1 cup honey roaste pecans, roughly choopped</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
 
 class MySpecialFooter extends React.Component {
   render() {
@@ -65,12 +88,10 @@ export default class PutEmAllTogether extends React.Component {
   render() {
       return (
         <div>  
-         <MyCoolHeader name="dani" />
-  
-        
-
+          <MyCoolHeader name="dani" />
           <MyAmazingArticle />
           <MySpecialFooter phone="345-456-5431" />
+          <RecipeList />
         </div>
       );
   }
