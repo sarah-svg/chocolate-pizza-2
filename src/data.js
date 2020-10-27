7a5c5abd1d419cdf6a2636bb3ecc368ee0faccf3
sarah-svg <kjskillen@gmail.com>
2020-10-26 17:11:26 -0700

    data.js

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const list1 = [
+   "1 1/2 cups milk",
+   "1/2 cups mascrarpone",
+   "1/2 tsp pink salt",
+   "1lb Black Mission Figs",
+   "1/2 cup brown sugar",
+   "2-4 tbsp water",
+
+]
+
+
+export const list2 = [
+"1 1/2 cups heavy cream",
+   "1/3 granulated sugar",
+   "2 egg yolks",
+   "lemon, juice",
+   "2 tbsp butter",
+   "1 cup honey roasted pecans, roughly chopped"
+]
